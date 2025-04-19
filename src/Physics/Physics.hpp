d8c3170a341830af470dfd8615fbb0a917c479ee
Farhad Hasanli <hasanli2@illinois.edu>
2025-04-19 14:58:28 -0500

    Redefined physics constants to be members of PhysicsConstants struct instead of global variables.

--- src/Physics/Physics.hpp
+++ src/Physics/Physics.hpp
@@ -7,31 +7,58 @@ namespace Prandtl
 
 using namespace mfem;
 
-extern const real_t gamma;
-extern const real_t gammaInverse;
-extern const real_t gammaP1;
-extern const real_t gammaM1;
-extern const real_t gammaP1Inverse;
-extern const real_t gammaM1Inverse;
-extern const real_t gamma_gammaM1Inverse; // gamma * gammaM1Inverse;
-extern const real_t gammaM1_gammaInverse; // gammaM1 * gammaInverse; 
-
-extern const real_t Pr;
-extern const real_t PrInverse;
-
-extern const real_t R_gas;
-extern const real_t cp;
-
-const real_t mu_bulk = 2.0 / 3.0;
-const real_t mu0 = 1.716e-5;
-const real_t T0 = 273.15;
-const real_t Ts = 110.4;
-const real_t T0pTs = T0 + Ts;
-
-inline real_t ComputeViscosity(const real_t &rho, const real_t &p)
+struct PhysicsConstants
 {
-    real_t T = p / (rho * R_gas);
-    return mu0 * T0pTs / (T + Ts) * (T / T0) * std::sqrt(T / T0);
-}
+    const real_t gamma;
+    const real_t gammaInverse;
+    const real_t gammaP1;
+    const real_t gammaM1;
+    const real_t gammaP1Inverse;
+    const real_t gammaM1Inverse;
+    const real_t gamma_gammaM1Inverse; // gamma * gammaM1Inverse;
+    const real_t gammaM1_gammaInverse; // gammaM1 * gammaInverse;
+
+    const real_t Pr;
+    const real_t PrInverse;
+    const real_t R_gas;
+    const real_t cp;
+    const real_t mu;
+
+    PhysicsConstants(real_t gamma, real_t Pr, real_t R_gas, real_t mu)
+      : gamma(gamma), Pr(Pr), R_gas(R_gas), mu(mu),
+        gammaInverse(1.0 / gamma), gammaM1(gamma - 1.0), gammaP1(gamma + 1.0),
+        gammaM1Inverse(1.0 / gammaM1), gammaP1Inverse(1.0 / gammaP1),
+        gammaM1_gammaInverse(gammaM1 * gammaInverse), gamma_gammaM1Inverse(gamma * gammaM1Inverse),
+        PrInverse(1.0 / Pr), cp(gamma_gammaM1Inverse * R_gas) {}
+
+    const real_t mu_bulk = 2.0 / 3.0;
+    const real_t mu0 = 1.716e-5;
+    const real_t T0 = 273.15;
+    const real_t Ts = 110.4;
+};
+
+// extern const real_t gamma;
+// extern const real_t gammaInverse;
+// extern const real_t gammaP1;
+// extern const real_t gammaM1;
+// extern const real_t gammaP1Inverse;
+// extern const real_t gammaM1Inverse;
+// extern const real_t gamma_gammaM1Inverse; // gamma * gammaM1Inverse;
+// extern const real_t gammaM1_gammaInverse; // gammaM1 * gammaInverse; 
+
+// extern const real_t Pr;
+// extern const real_t PrInverse;
+
+// extern const real_t R_gas;
+// extern const real_t cp;
+
+// extern const real_t mu;
+
+// const real_t mu_bulk = 2.0 / 3.0;
+// const real_t mu0 = 1.716e-5;
+// const real_t T0 = 273.15;
+// const real_t Ts = 110.4;
+// const real_t T0pTs = T0 + Ts;
+
 
 }
